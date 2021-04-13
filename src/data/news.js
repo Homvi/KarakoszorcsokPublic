586b26e62b449673c5f10a94184dc8092e92182a
HonvedoAdam <honvedoadamkj@gmail.com>
2021-04-13 09:31:33 +0200

    add dummy data

---
+++ src/data/news.js
@@ -0,0 +1,34 @@
+//Types can be default, video, img.
+
+export const news = [
+  {
+    id: 1,
+    title: "Icredibly long title for test how lon it can be",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    date: "2012-03-19",
+    src: "https://picsum.photos/200",
+    type: "default",
+    category: "oltás",
+  },
+  {
+    id: 1,
+    title: "Icredibly long title for test how lon it can be medium",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    date: "2012-04-19",
+    src: "https://picsum.photos/200",
+    type: "default",
+    category: "oltás",
+  },
+  {
+    id: 1,
+    title: "Icredibly long title for test how lon it can be latest",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting ",
+    date: "2012-05-19",
+    src: "https://picsum.photos/200",
+    type: "default",
+    category: "oltás",
+  },
+];
