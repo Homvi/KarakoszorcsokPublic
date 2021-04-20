aaa1b93068cc51266ef4dfbdd82c6d666fd01dbb
HonvedoAdam <honvedoadamkj@gmail.com>
2021-04-20 16:07:46 +0200

    revove nused var

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -3,15 +3,15 @@ import React from "react";
 import FirstGallery from "../components/FirstGallery";
 import SecondGallery from "../components/SecondGallery";
 import { OldImages, imagesBig } from "../data/galleryData";
-import { LazyLoadComponent } from "react-lazy-load-image-component";
 
-console.log(OldImages);
+
 const Gallery = () => {
   return (
     <Grid
       style={{
         minHeight: "calc(100vh - 70px)",
-        backgroundImage: "repeating-linear-gradient(284deg, transparent 0px, transparent 1px,rgb(251,251,251) 1px, rgb(251,251,251) 3px),repeating-linear-gradient(14deg, transparent 0px, transparent 1px,rgb(251,251,251) 1px, rgb(251,251,251) 3px),linear-gradient(90deg, rgb(222,222,222),rgb(222,222,222))",
+        backgroundImage:
+          "repeating-linear-gradient(284deg, transparent 0px, transparent 1px,rgb(251,251,251) 1px, rgb(251,251,251) 3px),repeating-linear-gradient(14deg, transparent 0px, transparent 1px,rgb(251,251,251) 1px, rgb(251,251,251) 3px),linear-gradient(90deg, rgb(222,222,222),rgb(222,222,222))",
       }}
       container
       direction="column"
