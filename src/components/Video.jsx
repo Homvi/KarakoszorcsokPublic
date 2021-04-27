ec84764fa5ac45ae42ceea7663972e1125c45889
HonvedoAdam <honvedoadamkj@gmail.com>
2021-04-27 09:13:30 +0200

    add controls

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player'
 const Video = () => {
     return (
         <div style={{ display:"flex", justifyContent:"center", width:"80%" ,margin:"0 auto"}} >
-            <ReactPlayer widt="100%"  url="https://youtu.be/CpZWHk6kCD8" />
+            <ReactPlayer widt="100%" controls="true"	 url="https://youtu.be/CpZWHk6kCD8" />
         </div>
     )
 }
