1d886c71a6db20e7ac201f2630faca3cf78ed8df
HonvedoAdam <honvedoadamkj@gmail.com>
2021-04-27 09:44:50 +0200

    play video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,8 +3,8 @@ import ReactPlayer from 'react-player'
 
 const Video = () => {
     return (
-        <div style={{ display:"flex", justifyContent:"center", width:"80%" ,margin:"0 auto"}} >
-            <ReactPlayer widt="100%" controls="true"	 url="https://youtu.be/CpZWHk6kCD8" />
+        <div style={{ display:"flex", zIndex:200 , justifyContent:"center", width:"92%" ,margin:"0 auto"}} >
+            <ReactPlayer widt="100%" controls={true} playing={true} volume={0.5} muted={true}	 url="https://youtu.be/CpZWHk6kCD8" />
         </div>
     )
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -8,10 +8,14 @@ const Welcome = () => {
             <Grid item align="center" xs={10} md={8} container>
                 <h2 style={{ margin: "10px auto" }} >Polgármesteri köszöntő</h2>
                 <div className="wecome-content">
-                    <p>
-                        Isten hozta önöket Karakószörcsök Község hivatalos honlapján!<br /> Megtiszteltetés számomra, hogy a település polgármestereként, önkormányzatunk nevében köszönthetek minden érdeklődőt.
+                    <p style={{ textAlign:"justify" }} >
+                        <Typography variant="subtitle2"  style={{ marginBottom:"5px", display:"flex", justifyContent:"center"}}>
+                            Isten hozta önöket Karakószörcsök Község hivatalos honlapján! 
+                        </Typography>
+                          Megtiszteltetés számomra, hogy a település polgármestereként, önkormányzatunk nevében köszönthetek minden érdeklődőt.
+                      
           Községünk Magyarország Dunántúli részén, a Bakony alján, Veszprém megyében a Somló és a Ság hegy között bújik meg. A falu fekvése, jó levegője, csöndes hangulata ideális a pihenni és kikapcsolódásra vágyóknak, a munkából hazatérőknek és a helyben lakóknak egyaránt.
-          Arra törekszünk, hogy honlapunk minden fontos, hasznos helyi információt tartalmazzon, és remélem, hogy ezeknek Önök is hasznát veszik.<br />
+          Arra törekszünk, hogy honlapunk minden fontos, hasznos helyi információt tartalmazzon, és remélem, hogy ezeknek Önök is hasznát veszik.
           Remélem, a honlap böngészése után kedvet érez majd, hogy személyesen is megismerkedjen településünkkel és az itt élő emberekkel!
                     </p>
                     <Typography style={{ margin: "20px" }} >
